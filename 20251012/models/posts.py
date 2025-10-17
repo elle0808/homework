e3d72cdb5eb3ef1aee46defa76a8c99f7e499ede
elle0808 <57534238+elle0808@users.noreply.github.com>
2025-10-17 22:38:54 +0800

    Add files via upload

---
+++ 20251012/models/posts.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text
+from .base import Base
+
+class PostDB(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(primary_key=True) 
+    slug: Mapped[str] = mapped_column(String(200), nullable=False, unique=True)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    author: Mapped[str] = mapped_column(String(100), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+
+    def __repr__(self):
+        return f"Post(id={self.id}, slug={self.slug}, title={self.title})"
