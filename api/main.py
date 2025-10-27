c0369fbebb9822b5c78e6186d4348afad3c7a5e3
elle0808 <57534238+elle0808@users.noreply.github.com>
2025-10-27 19:28:37 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -68,5 +68,5 @@ def serve_index():
 if __name__ == "__main__":
     # 運行在 8000 埠
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
                
