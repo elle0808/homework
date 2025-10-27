b420af6768f191d8521571a71458ab85bb8f300d
elle0808 <57534238+elle0808@users.noreply.github.com>
2025-10-27 19:33:42 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
+from starlette.responses import FileResponse
 import logging
 import os
 
